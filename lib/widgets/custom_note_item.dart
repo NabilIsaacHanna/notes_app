8e14c332a1e9da5ba1a982a1f033b1f24d1eb3ba
NabilIsaacHanna <nabilisaac5@gmail.com>
2024-09-03 18:28:55 +0300

    create note list view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class NotesItem extends StatelessWidget {
-  const NotesItem({super.key});
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
