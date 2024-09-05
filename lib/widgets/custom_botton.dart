f3bcf081e662b1b6e9ba67258aa20ed1c0b41d80
NabilIsaacHanna <nabilisaac5@gmail.com>
2024-09-05 20:07:09 +0300

    add add note cubit to add note bottom sheet

--- lib/widgets/custom_botton.dart
+++ lib/widgets/custom_botton.dart
@@ -13,7 +13,7 @@ class CustomButton extends StatelessWidget {
         height: 55,
         decoration: BoxDecoration(
             color: kPrimaryColor, borderRadius: BorderRadius.circular(16)),
-        child: Center(
+        child: const Center(
           child: Text(
             'Add',
             style: TextStyle(
